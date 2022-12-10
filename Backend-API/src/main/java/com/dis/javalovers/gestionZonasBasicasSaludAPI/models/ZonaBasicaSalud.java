6ae442e65aa7c964dbddf795dd9671118c6e6cc3
Willy1123 <guillermoblascoperez@gmail.com>
2022-12-10 05:40:46 +0100

    Creada la clase para modelar los datos recibidos del JSON ZonasBasicasSalud

---
+++ Backend-API/src/main/java/com/dis/javalovers/gestionZonasBasicasSaludAPI/models/ZonaBasicaSalud.java
@@ -0,0 +1,29 @@
+package com.dis.javalovers.gestionZonasBasicasSaludAPI.models;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Clase para modelar los datos recibidos del JSON ZonasBasicasSalud
+ */
+@AllArgsConstructor @NoArgsConstructor @Getter @Setter
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
+public class ZonaBasicaSalud {
+    @JsonProperty("Código geometria")
+    private String codigo_geometria;
+    @JsonProperty("Zona básica de salud")
+    private String zona_basica_salud;
+    @JsonProperty("Tasa de incidencia acumulada (últimos 14 dias)")
+    private Float tasa_incidencia_acumulada_ultimos_14dias;
+    @JsonProperty("Tasa incidencia acumulada (total)")
+    private Float tasa_incidencia_acumulada_total;
+    @JsonProperty("Casos confirmados totales")
+    private int casos_confirmados_totales;
+    @JsonProperty("Fecha del Informe")
+    private String fecha_informe;
+
+}
