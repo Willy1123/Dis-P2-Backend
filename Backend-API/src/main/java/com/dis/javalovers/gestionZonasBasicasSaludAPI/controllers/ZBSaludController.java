07bb1910073ccfd1e0b0b20a2868801badd6d5fd
Willy1123 <guillermoblascoperez@gmail.com>
2022-12-10 19:34:20 +0100

    Mapeo del POST Creado. - La clase modelo hay que cambiarla ya que fecha_informe debe ser tipo Date en vez de String. Pero para probar y hacer también el delete, seguirá como String

--- Backend-API/src/main/java/com/dis/javalovers/gestionZonasBasicasSaludAPI/controllers/ZBSaludController.java
+++ Backend-API/src/main/java/com/dis/javalovers/gestionZonasBasicasSaludAPI/controllers/ZBSaludController.java
@@ -4,10 +4,9 @@ import com.dis.javalovers.gestionZonasBasicasSaludAPI.models.ZonaBasicaSalud;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -38,5 +37,18 @@ public class ZBSaludController {
             }
         }
         return null;
+    }
+
+    // POST
+    @PostMapping(value = "/ZonaBasicaSalud",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Añade una nueva Zona Básica de Salud al Sistema",
+            description = "Añade una nueva zona básica de salud siempre que los datos introducidos sean correctos")
+    @ResponseStatus(HttpStatus.CREATED)
+    public static ResponseEntity<ArrayList<ZonaBasicaSalud>> addZBS(@RequestBody ZonaBasicaSalud nuevaZBS) {
+        LectorJson.datos.data.add(nuevaZBS);
+        return new ResponseEntity<>(LectorJson.datos.data, HttpStatus.CREATED);
+
     }
 }
