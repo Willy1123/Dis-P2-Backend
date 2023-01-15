9a409d2ac1fe94ff9308fa69abfff7a90510f4fc
Cristian Sevilla <c.sevi@outlook.es>
2023-01-15 23:19:48 +0100

    Problemas resueltos, nuevos comentarios añadidos y funcionalidad en post para modificar o crear una nueva zona de salud para mayores de 60

--- Backend-API/src/main/java/com/dis/javalovers/gestionZonasBasicasSaludAPI/controller/ZBSMayoresController.java
+++ Backend-API/src/main/java/com/dis/javalovers/gestionZonasBasicasSaludAPI/controller/ZBSMayoresController.java
@@ -23,21 +23,18 @@ public class ZBSMayoresController {
 
     private JsonDAO_60 jsonDAO_60 = JsonDAO_60.getInstance();
 
-    // GET
+    // GET, lista completa
     @GetMapping("/ZonasBasicasSaludMayores60")
-    @Operation(summary = "Devuelve una lista de todas las Zonas Básicas de Salud",
-            description = "Devuelve los datos pertinentes de las zonas básicas de salud. No ejecutar en la web de Swagger ya que crashea la web")
+    @Operation(summary = "Devuelve una lista de todas las Zonas Básicas de Salud para mayores de 60", description = "Devuelve los datos pertinentes de las zonas básicas de salud. No ejecutar en la web de Swagger ya que crashea la web")
     public List<ZonaBasicaSalud_60> zBSalud60_GET() {
         return jsonDAO_60.leerJsonZBS_60();
     }
 
-    // GET
+    // GET, busqueda por codigo_geometria
     @GetMapping("/ZonaBasicaSaludMayores60/{codigo_geometria}")
-    @Operation(summary = "Devuelve Zonas Básicas de Salud por Código de Geometría",
-            description = "Devuelve los datos pertinentes de la zona básica de salud seleccionada")
+    @Operation(summary = "Devuelve Zonas Básicas de Salud por Código de Geometría", description = "Devuelve los datos pertinentes de la zona básica de salud seleccionada")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "OK!"),
-            @ApiResponse(responseCode = "404", description = "Not OK! - Not Found")
+            @ApiResponse(responseCode = "200", description = "OK!"), @ApiResponse(responseCode = "404", description = "Not OK! - Not Found")
     })
     public static ResponseEntity<List<ZonaBasicaSalud_60>> zBSaludId_GET(@PathVariable("codigo_geometria") String codigo) {
         try {
@@ -62,29 +59,53 @@ public class ZBSMayoresController {
 
 
     // POST
-    @PostMapping(value = "/ZonaBasicaSaludMayores_60",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(summary = "Añade una nueva Zona Básica de Salud para Mayores de 60 al Sistema",
-            description = "Añade una nueva zona básica de salud para mayores de 60 siempre que los datos introducidos sean correctos")
+    @PostMapping(value = "/ZonaBasicaSaludMayores_60", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Añade una nueva Zona Básica de Salud para Mayores de 60 al Sistema", description = "Añade una nueva zona básica de salud para mayores de 60 siempre que los datos introducidos sean correctos")
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<ArrayList<ZonaBasicaSalud>> addZBS(@RequestBody int indice, ZonaBasicaSalud_60 datosNuevaZBS) {
+    public ResponseEntity<ArrayList<ZonaBasicaSalud_60>> addZBS(@RequestBody boolean nuevoCampo, int indice, ZonaBasicaSalud_60 datosNuevaZBS) {
 
-        // creamos una lista tipo ZonaBasicaSalud donde guardamos el json leído.
+        // creamos una lista tipo ZonaBasicaSalud_60 donde guardamos el json leído.
         List<ZonaBasicaSalud_60> listaZBS = jsonDAO_60.leerJsonZBS_60();
+        // creamos un elemento de tipo ZonaBasicaSalud_60 para guardar el nuevo
         ZonaBasicaSalud_60 nuevaZBS = new ZonaBasicaSalud_60();
         // actualizamos los datos del objeto de la lista seleccionado
-        nuevaZBS.setCodigo_geometria(datosNuevaZBS.getCodigo_geometria());
+        //nuevaZBS.setCodigo_geometria(datosNuevaZBS.getCodigo_geometria());
         nuevaZBS.setZona_basica_salud(datosNuevaZBS.getZona_basica_salud());
         nuevaZBS.setCasos_confirmados_P60mas_ultimos_14dias(datosNuevaZBS.getTasa_incidencia_acumulada_P60mas_ultimos_14dias());
         nuevaZBS.setCasos_confirmados_P60mas_ultimos_14dias(datosNuevaZBS.getCasos_confirmados_P60mas_ultimos_14dias());
         nuevaZBS.setFecha_informe(datosNuevaZBS.getFecha_informe());
-        // añadimos los cambios a la lista
-        listaZBS.set(indice, nuevaZBS);
+        boolean found = false;
+        int max_cod_geo = 0;
+
+        for(ZonaBasicaSalud_60 zn : listaZBS) {
+            if(zn.getZona_basica_salud().equals(nuevaZBS.getZona_basica_salud()) ) {
+                nuevaZBS.setCodigo_geometria(zn.getCodigo_geometria());
+                found = true;
+                if(Integer.parseInt(zn.getCodigo_geometria()) > max_cod_geo) {
+                    max_cod_geo = Integer.parseInt(zn.getCodigo_geometria());
+                }
+                break; // Si hemos encontrado una coincidencia terminamos el for
+            }
+        }
+
+        if(!found) { // Si no hemos encontrado ninguna coincidencia, cogeremos el codigo de geometria mayor y le sumaremos 1 para hacer el nuevo elemento el final
+            nuevaZBS.setCodigo_geometria(String.valueOf(max_cod_geo+1));
+        }
+
+        if (nuevoCampo) {
+            // añadimos el nuevo objeto a la lista si se dio al boton de crear en el for
+            listaZBS.add(nuevaZBS);
+        }
+
+        // en caso contrario, simplemente estamos modicicando un elemento existente
+        else {
+            // por lo que modificamos los datos del elemento seleccionado
+            listaZBS.set(indice, nuevaZBS);
+        }
 
         // guardamos el json con los cambios realizados
         jsonDAO_60.guardarJsonZBS_60(listaZBS);
-        System.out.println("guardado en el json correctamente");
+        System.out.println("Guardado en el json correctamente");
         return null;
     }
 
